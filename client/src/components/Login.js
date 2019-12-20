801e6f5a669902f9d5e861780b54d55afd52770f
michelangelo17 <michelangelo.markus@gmail.com>
2019-12-20 09:01:29 -0800

    login working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,54 @@
 import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+import { useHistory } from 'react-router-dom'
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  const history = useHistory()
+  const postLogin = values =>
+    axiosWithAuth()
+      .post('/login', values)
+      .then(res => localStorage.setItem('token', res.data.payload))
+      .catch(err => console.log(err))
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <Formik
+        initialValues={{
+          username: '',
+          password: '',
+        }}
+        onSubmit={async (values, { resetForm }) => {
+          await postLogin(values)
+          history.push('/home')
+          resetForm()
+        }}
+        validationSchema={Yup.object().shape({
+          username: Yup.string().required(`can't be empty`),
+          password: Yup.string().required(`can't be empty`),
+        })}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <ErrorMessage name='username' component='p' />
+            <Field
+              type='username'
+              name='username'
+              placeholder='enter username'
+            />
+            <ErrorMessage name='password' component='p' />
+            <Field
+              type='password'
+              name='password'
+              placeholder='enter password'
+            />
+            <button type='submit' disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
     </>
   )
 }
